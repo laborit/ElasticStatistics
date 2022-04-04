764b7d8c702ce8f1ddc184aac65b4eb830e661a0
Sergei Sergeev <laborit2@gmail.com>
2022-04-04 10:21:03 +0300

    04.04.2022

--- ElasticStatistics/delete_indeces.py
+++ ElasticStatistics/delete_indeces.py
@@ -6,17 +6,10 @@ from elasticsearch import Elasticsearch
 def connect_elasticsearch():
     _es = None
     _es = Elasticsearch([{'host': '10.0.2.29', 'port': 9200}])
-    # if _es.ping():
-    #     print('Yay Connect')
-    # else:
-    #     print('Awww it could not connect!')
     return _es
 
 if __name__ == '__main__':
-    # start_date, end_date = datetime.date(2020,7, 1), datetime.date.today()
-    # day_count = (end_date - start_date).days + 1
     logging.basicConfig(level=logging.ERROR)
     es=connect_elasticsearch()
-    es.indices.delete(index='*202006*', ignore=[400, 404])
+    es.indices.delete(index='*202102*', ignore=[400, 404])
 
-    # data.to_csv('out.csv')
